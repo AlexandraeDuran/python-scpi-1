65a5bc50dddaa2914c6b074a89730f04e9a24a16
Alexander Chemeris <Alexander.Chemeris@gmail.com>
2015-05-23 00:18:10 -0400

    cmd57: New commands implemented.

--- scpi/devices/cmd57.py
+++ scpi/devices/cmd57.py
@@ -13,11 +13,61 @@ class cmd57(scpi_device):
 
     def ask_installed_options(self):
         """ List installed option """
-        return self.scpi.ask_str("*OPT?")
+        return self.scpi.ask_str_list("*OPT?")
 
     def ask_io_used(self):
-        """ Inputs and outputs used """
+        """ 2.1 Inputs and outputs used """
         return self.scpi.ask_str("ROUT:IOC?")
+
+    def ask_bts_mcc(self):
+        """ 2.2.1 Detected BTS MCC """
+        return self.scpi.ask_int("SENSE:SIGN:IDEN:MCC?")
+
+    def ask_bts_mnc(self):
+        """ 2.2.1 Detected BTS MNC """
+        return self.scpi.ask_int("SENSE:SIGN:IDEN:MNC?")
+
+    def ask_bts_bsic(self):
+        """ 2.2.1 Detected BTS BSIC """
+        return self.scpi.ask_int("SENSE:SIGN:BSIC?")
+
+    def ask_bts_ccch_arfcn(self):
+        """ 2.2.2 Configured CCCH ARFCN """
+        return self.scpi.ask_int("CONF:CHAN:BTS:CCCH:ARFCN?")
+
+    def ask_bts_tch_arfcn(self):
+        """ 2.2.2 Configured TCH ARFCN """
+        return self.scpi.ask_int("CONF:CHAN:BTS:TCH:ARFCN?")
+
+    def ask_bts_tch_ts(self):
+        """ 2.2.2 Configured TCH timeslot """
+        return self.scpi.ask_int("CONF:CHAN:BTS:TCH:SLOT?")
+
+    def ask_bts_tsc(self):
+        """ 2.2.2 Configured BTS TSC """
+        return self.scpi.ask_int("CONF:CHAN:BTS:TSC?")
+
+    def ask_ban_tsc(self):
+        """ 2.3 Burst Analysis (Module testing) TSC """
+        return self.scpi.ask_int("CONF:CHAN:BANalysis:TSC?")
+
+    def ask_test_mode(self):
+        """ 2.4 Test mode
+            Supported values:
+              NONE        - No tes mode (switch on state)
+              MANual      - BTS test aka signaling test
+              MODultest   - Module test (same as BAN?)
+              BANalysis   - Burst analysis (same as MOD?)
+              RFM         - RF generator (same as RFG?)
+              RFGenerator - RF generator (same as RFM?)
+              IQSPec      - IQ spectrum (requires hardware option)
+        """
+        return self.scpi.ask_str("PROCedure:SEL?")
+
+    def ask_dev_state(self):
+        """ 9.1 Current Device State """
+        return self.scpi.ask_str("STATus:DEVice?")
+
 
 def rs232(port, **kwargs):
     """Quick helper to connect via RS232 port"""
