991bfffb46ab499d03392aef106b7a8a053ed70d
Alexander Chemeris <Alexander.Chemeris@gmail.com>
2015-05-22 22:48:14 -0400

    cmd57: Checking in an example program.

---
+++ examples/cmd57-example.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python -i
+import os,sys
+# Add the parent dir to search paths
+#libs_dir = os.path.join(os.path.dirname( os.path.realpath( __file__ ) ),  '..')
+#if os.path.isdir(libs_dir):                                       
+#    sys.path.append(libs_dir)
+
+from scpi.devices import cmd57
+import atexit
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print "Usage:"
+        print "  python -i cmd57-example.py /dev/ttyUSB0"
+        sys.exit(1)
+    # Then put to interactive mode
+    os.environ['PYTHONINSPECT'] = '1'
+    dev = cmd57.rs232(sys.argv[1], rtscts=True)
+    atexit.register(dev.quit)
+
+    print dev.identify()
+    # List options
+    print dev.ask_installed_options()
+    # which port are we using?
+    print dev.ask_io_used()
