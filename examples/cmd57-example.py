6a89f9855a04af51cd5387d4e0d47e4ba9f64c44
Alexander Chemeris <Alexander.Chemeris@gmail.com>
2015-05-23 00:18:30 -0400

    cmd57: Example updated to to show more configuration on start.

--- examples/cmd57-example.py
+++ examples/cmd57-example.py
@@ -18,8 +18,16 @@ if __name__ == '__main__':
     dev = cmd57.rs232(sys.argv[1], rtscts=True)
     atexit.register(dev.quit)
 
-    print "Tester version: %s" % " ".join(dev.identify())
-    print "Installed options: %s" % dev.ask_installed_options()
-    print "RF input/output port configuration: %s " % dev.ask_io_used()
+    print "Tester version:       %s" % " ".join(dev.identify())
+    print "Installed options:    %s" % " ".join(dev.ask_installed_options())
+    print "RF input/output port: %s" % dev.ask_io_used()
+    print "BTS CCCH ARFCN:       %d" % dev.ask_bts_ccch_arfcn()
+    print "BTS TCH ARFCN:        %d" % dev.ask_bts_tch_arfcn()
+    print "BTS TCH timeslot:     %d" % dev.ask_bts_tch_ts()
+    print "BTS TSC:              %d" % dev.ask_bts_tsc()
+    print "Module test TSC:      %d" % dev.ask_ban_tsc()
+    print "Current test mode:    %s" % dev.ask_test_mode()
+    print "Current device state: %s" % dev.ask_dev_state()
     print
     print "Expecting your input now"
+
